3d6fbd905a53cd427d94a58f221d5c107c4bd8ee
이광용 <rhkdrmfh@gmail.com>
2018-03-20 14:02:27 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 이광용 on 2018. 3. 20..
+//  Copyright © 2018년 이광용. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
